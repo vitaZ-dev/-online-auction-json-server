ed38b0daaee651286d7c0b61f2ffbd16020f389f
vitaZ-dev <vitaminzdev@gmail.com>
2025-03-21 19:36:12 +0900

    코드 임시 롤백

--- api/server.js
+++ api/server.js
@@ -6,11 +6,10 @@ const server = jsonServer.create();
 // Uncomment to allow write operations
 const fs = require("fs");
 const path = require("path");
-// const filePath = path.join("db.json");
-const filePath = path.join("/tmp", "db.json");
-// const data = fs.readFileSync(filePath, "utf-8");
-const db = fs.writeFileSync(filePath, JSON.stringify(data));
-const router = jsonServer.router(JSON.parse(db));
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
 // Comment out to allow write operations
 // const router = jsonServer.router("db.json");
